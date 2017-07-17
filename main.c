979154f28d5c20146ed1f29f87c53457b48011d4
Krystian Kaniewski <yonji94@gmail.com>
2017-07-17 11:09:21 +0200

    Menu

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+void menu() {
+	printf("Kebab factory: \n\n");
+	printf("Polski - flaga pl\n");
+	printf("English - flag en\n");
+}
+
+int main (int argc, const char ** argv) {
+	menu();
+}
